ca2bd1c583f431cc75245122bcb84b7400480e6b
Nurislam Ab <you@example.com>
2020-10-15 00:33:04 +0500

    Make the basic implementation of the App components wrapping BooksList and BooksForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
+      <BooksList />
+      <BooksForm />
     </div>
   );
 }
