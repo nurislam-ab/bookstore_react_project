e5421ab46a78801d7aedfd52c48f8bc9da1c9271
Nurislam Ab <you@example.com>
2020-10-15 13:15:53 +0500

    Refactor app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../App.css';
+import BookList from '../containers/BookList';
+import BookForm from '../containers/BooksForm';
 
-function App() {
-  return (
-    <div className="App">
-      <BooksList />
-      <BooksForm />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <BookList />
+    <BookForm />
+  </div>
+);
 
 export default App;
