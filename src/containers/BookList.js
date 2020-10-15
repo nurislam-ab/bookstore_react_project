10a9ef27a807cceb5092233bb512187d09c4fb64
Nurislam Ab <you@example.com>
2020-10-15 10:22:31 +0500

    Connect BookList to the Redux store

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const BooksList = (() => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>bookId</td>
-          <td>title</td>
-          <td>category</td>
-        </tr>
-      </tbody>
-    </table>
-  )
+const BooksList = ({ books }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>bookId</td>
+        <td>title</td>
+        <td>category</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+const mapStateToProps = state => ({
+  books: state.books,
 });
 
-export default BooksList;
+export default connect(mapStateToProps, null)(BooksList);
