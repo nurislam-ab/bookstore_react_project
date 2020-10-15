9b3b92ff7f16d6f61db73b0e159c0a20e0721a98
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-15 10:22:47 +0500

    Add Book component

---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Book = ({ bookId, title, category }) => (
+  <tr>
+    <td>{bookId}</td>
+    <td>{title}</td>
+    <td>{category}</td>
+  </tr>
+);
+
+export default Book;
