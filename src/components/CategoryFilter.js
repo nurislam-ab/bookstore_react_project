26c94b7a130d1ce3502bb6c21366c1330bb72e89
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-16 18:45:57 +0500

    Add method to handle filter and import categories list

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import categories from '../const/categories';
+import PropTypes from 'prop-types';
+import categoriesList from '../const/categories';
 
-const CategoryFilter = () => (
-  <select>
-    <option key="All" value="All">All</option>
-    {categories.map(category => (
-      <option key={category} value={category}>
-        {category}
-      </option>
-    ))}
-  </select>
-);
+const CategoryFilter = ({ onFilter }) => {
+  const categories = categoriesList;
+
+  const handleFilter = ({ target }) => {
+    onFilter(target.value);
+  };
+
+  return (
+    <select name="category" onChange={handleFilter}>
+      <option key="All" value="All">All</option>
+      {categories.map(category => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+CategoryFilter.propTypes = {
+  onFilter: PropTypes.func.isRequired,
+};
 
 export default CategoryFilter;
