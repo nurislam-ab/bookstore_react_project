be498a8572ca58ad08b307606708280eed4173b7
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-17 11:42:54 +0500

    Integrate the new design structure  for the book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Book.scss';
 
 const Book = ({ book, removeBook }) => (
-  <tr>
-    <td>{book.bookId}</td>
-    <td>{book.title}</td>
-    <td>{book.category}</td>
-    <td>
-      <button
-        type="button"
-        onClick={() => removeBook(book)}
-      >
-        Remove Book
-      </button>
-    </td>
-  </tr>
+    <div className="book-preview-card">
+      <div className="book-preview-info">
+        <span className="book-category">{book.category}</span>
+        <h2 className="book-title">{book.title}</h2>
+        <a href="https://polar-citadel-29411.herokuapp.com/" className="book-author">Suzanne Collins</a>
+        <div className="book-preview-btns">
+          <div className="btn-wrapper">
+            <button type="button">Comments</button>
+          </div>
+          <div className="btn-wrapper">
+            <button
+              type="button"
+              onClick={() => removeBook(book)}
+            >
+              Remove Book
+            </button>
+          </div>
+          <div className="btn-wrapper">
+            <button type="button">Edit</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="reading-progress">
+        <div id="c100" className="progress">
+          <div className="slice">
+            <div className="bar" />
+            <div className="fill" />
+          </div>
+        </div>
+        <div className="progress-info">
+          <span className="progress-percent">
+            75
+            %
+          </span>
+          <span className="progress-text">Completed</span>
+        </div>
+      </div>
+
+      <div className="reading-data">
+        <span className="chapter-label">Current chapter</span>
+        <strong className="chapter">Chapter 17</strong>
+        <button type="button" className="update-progress-btn">Update progress</button>
+      </div>
+    </div>
 );
 
 Book.propTypes = {
