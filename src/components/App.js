86b3bd8c379ba5974c85d51176b58b299db71482
Nurislam Ab <abdurasulovnurislam@gmail.com>
2020-10-17 11:38:41 +0500

    Import header component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import '../App.css';
 import BookList from '../containers/BookList';
 import BookForm from '../containers/BooksForm';
+import Header from './Header';
 
 const App = () => (
   <div className="App">
+    <Header />
     <BookList />
     <BookForm />
   </div>
