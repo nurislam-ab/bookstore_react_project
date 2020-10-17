da2d1a8a72e744cc13bb7b80cc85a6c5c489db92
Nurislam Ab <you@example.com>
2020-10-17 11:50:32 +0500

    Imort reset css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import './reset.css';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers/index';
