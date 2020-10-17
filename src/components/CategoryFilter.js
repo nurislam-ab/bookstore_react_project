0200ab127db5ffcd37863b260e4bcd10b1b446df
Nurislam Ab <you@example.com>
2020-10-17 13:32:55 +0500

    Add styles for the category filter component

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../CategoryFilter.scss';
 import categoriesList from '../const/categories';
 
 const CategoryFilter = ({ onFilter }) => {
